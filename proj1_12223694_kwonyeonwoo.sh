d940f2e586090865e99d0243452144756ee5bbc3
yeonwoo <yeonyoo5969@gmail.com>
2023-10-27 22:48:41 +0900

    출력 수정

--- proj1_12223694_kwonyeonwoo.sh
+++ proj1_12223694_kwonyeonwoo.sh
@@ -4,12 +4,12 @@ read -p "User Name: " uname
 read -p "Student Number: " stunum
 
 echo "[ MENU ]\n
-1. Get the data of the movie identified by a specific 'movie id' from 'u.item'\n
-2. Get the data of action genre movies from 'u.item'\n
-3. Get the average 'rating' of the movie identified by specific 'movie id' from 'u.data'\n
-4. Delete the 'IMDb URL' from 'u.item'\n
-5. Get the data about users from 'u.user'\n
-6. Modify the format of 'release date' in 'u.item'\n
-7. Get the data of movies rated by a specific 'user id' from 'u.data'\n
-8. Get the average 'rating' of movies rated by users with 'age' between 20 and 29 and 'occupation' as 'programmer'\n
+1. Get the data of the movie identified by a specific 'movie id' from 'u.item'
+2. Get the data of action genre movies from 'u.item'
+3. Get the average 'rating' of the movie identified by specific 'movie id' from 'u.data'
+4. Delete the 'IMDb URL' from 'u.item'
+5. Get the data about users from 'u.user'
+6. Modify the format of 'release date' in 'u.item'
+7. Get the data of movies rated by a specific 'user id' from 'u.data'
+8. Get the average 'rating' of movies rated by users with 'age' between 20 and 29 and 'occupation' as 'programmer'
 9. Exit"
