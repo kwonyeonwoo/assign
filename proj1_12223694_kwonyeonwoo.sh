45db2b3740586707c89b9e3457cff90f379ae742
yeonwoo <yeonyoo5969@gmail.com>
2023-10-27 23:30:56 +0900

    add while and case

--- proj1_12223694_kwonyeonwoo.sh
+++ proj1_12223694_kwonyeonwoo.sh
@@ -1,9 +1,10 @@
 #! /bin/bash
 
-read -p "User Name: " uname
-read -p "Student Number: " stunum
 
-echo "[ MENU ]\n
+echo "--------------------------
+User Name: kwonyeonwoo
+Student Number: 12223694
+[ MENU ]
 1. Get the data of the movie identified by a specific 'movie id' from 'u.item'
 2. Get the data of action genre movies from 'u.item'
 3. Get the average 'rating' of the movie identified by specific 'movie id' from 'u.data'
@@ -12,4 +13,29 @@ echo "[ MENU ]\n
 6. Modify the format of 'release date' in 'u.item'
 7. Get the data of movies rated by a specific 'user id' from 'u.data'
 8. Get the average 'rating' of movies rated by users with 'age' between 20 and 29 and 'occupation' as 'programmer'
-9. Exit"
+9. Exit
+--------------------------"
+read -p "Enter your choice [ 1-9 ] " choice
+while [ $choice -ne 9 ]
+do
+	case $choice in
+	1)
+		echo "1" ;;
+	2)
+		echo "2" ;;
+	3)
+		echo "3" ;;
+	4) 
+		echo "4" ;;
+	5)
+		echo "5" ;;
+	6)
+		echo "6" ;;
+	7)
+		echo "7" ;;
+	8)
+		echo "8" ;;
+	esac
+read -p "Enter your choice [ 1-9 ] " choice
+done
+echo "Bye !"
